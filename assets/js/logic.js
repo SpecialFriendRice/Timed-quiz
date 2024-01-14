63006b393d660220000ee9bebfa650088f230c6b
SpecialFriendRice <corinneborgeaud@corinneimbp2022.home>
2024-01-14 15:38:06 +0000

    first pseudo code and scores.js added

---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+//QUESTIONS
+//add ordered list with four child list elements to div id questions
+//each child element needs to be a button
+//text of each button is an answer from the potential answers array (?)
+
+//HIGHSCORES
+//highscores <ol> needs five (?) topscore child li elements
+//function to be written for Clear Highscores button
+//highscores are saved optionally when game ends - make this choice a confirm?
+
+//TIMER
+//code needs to be written to start timer when start button is pressed
+//set max time (60 seconds?)
+//for each incorrect answer 10 seconds are deducted
+//when timer reaches zero, quiz ends (with an alert or different?) 
---
+++ assets/js/questions.js
@@ -0,0 +1,12 @@
+//unordered list for answers with randomisation?
+//answers stored in an array?
+
+var q1 = "How many beans make five?";
+var a1Arr = ["two", "three", "four", "five"];
+
+var q2 = "What type of animal has wings?";
+var a2Arr = ["shark", "warthog", "swan", "hedgehog"];
+
+var q3 = "What is brie?";
+var a3Arr = ["a garment", "a cheese", "a form of government", "a colour"];
+
