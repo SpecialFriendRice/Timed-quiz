bb3d58ad346d846fcba95bc6fc666e9ca53f472c
SpecialFriendRice <corinneborgeaud@corinneimbp2022.home>
2024-01-14 16:04:16 +0000

    More pseudocode added

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,5 @@
 //QUESTIONS
+//question goes in question-title h2
 //add ordered list with four child list elements to div id questions
 //each child element needs to be a button
 //text of each button is an answer from the potential answers array (?)
@@ -6,10 +7,30 @@
 //HIGHSCORES
 //highscores <ol> needs five (?) topscore child li elements
 //function to be written for Clear Highscores button
-//highscores are saved optionally when game ends - make this choice a confirm?
+//highscores are saved optionally when game ends - make this choice a confirm? No, this is not how the demo gif show it, there is no option, the user can only submit after entering initials in end-screen div and then is taken to Highscores page
+//code needs to be written to take user to Highscores page AND save the score in the Highscores table - can this simply be combined with an href in the button tag and function for onclick?
+//Highscores saved in localstorage (?)
 
 //TIMER
 //code needs to be written to start timer when start button is pressed
 //set max time (60 seconds?)
 //for each incorrect answer 10 seconds are deducted
 //when timer reaches zero, quiz ends (with an alert or different?) 
+
+
+//START
+//function for start button on click
+//questions div class changes from hide after start button pressed
+//questions div class goes back to hide after all questions answered OR time = 0
+//end-screen div class changes from hide after after all questions answered OR time = 0
+//end-screen div class does not need to go back to hide as goes to highscores page?
+
+//FEEDBACK
+//possible feedback responses written and put in array (?)
+//feedback responses appear in the feedback div when an answer button is clicked
+//when is class changed from hide?
+
+//SCORE
+//score is kept as running total (local storage?)
+//create a variable finalTally that feeds to end-screen div in final-score span
+//decide on points value for a correct answer (is the points value related to the amount of time left?)
